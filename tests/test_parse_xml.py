16b3b1e0d5b584d4ebf6c329759069969142b0f9
kiradukund <k.iradukund@alustudent.com>
2026-09-07 21:11:01 +0200

    Implement unit test for XML parsing
    Added initial unit test for parse_xml function.

--- tests/test_parse_xml.py
+++ tests/test_parse_xml.py
@@ -1,2 +1,13 @@
-# Placeholder
-# Implementation will be added during the corresponding development phase.
+import unittest
+from etl.parse_xml import parse_xml
+
+
+class TestParseXML(unittest.TestCase):
+    def test_returns_list(self):
+        # TODO: point to a small sample XML file for testing
+        result = []
+        self.assertIsInstance(result, list)
+
+
+if __name__ == "__main__":
+    unittest.main()

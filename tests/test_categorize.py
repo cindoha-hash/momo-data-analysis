bbec408533d419d55e2e0459d71fc4b05a223809
kiradukund <k.iradukund@alustudent.com>
2026-09-07 21:11:35 +0200

    Add unit tests for categorize_transaction function

--- tests/test_categorize.py
+++ tests/test_categorize.py
@@ -1,2 +1,16 @@
-# Placeholder
-# Implementation will be added during the corresponding development phase.
+import unittest
+from etl.categorize import categorize_transaction
+
+
+class TestCategorize(unittest.TestCase):
+    def test_deposit(self):
+        txn = {"message": "You have received a deposit of 1000 RWF"}
+        self.assertEqual(categorize_transaction(txn), "deposit")
+
+    def test_unknown(self):
+        txn = {"message": "Some random message"}
+        self.assertEqual(categorize_transaction(txn), "other")
+
+
+if __name__ == "__main__":
+    unittest.main()

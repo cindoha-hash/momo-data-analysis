d8642406542a7e9c49de64eba98753b066ac1a72
kiradukund <k.iradukund@alustudent.com>
2026-09-07 21:11:17 +0200

    Update test_clean_normalize.py

--- tests/test_clean_normalize.py
+++ tests/test_clean_normalize.py
@@ -1,2 +1,14 @@
-# Placeholder
-# Implementation will be added during the corresponding development phase.
+import unittest
+from etl.clean_normalize import normalize_amount, normalize_phone
+
+
+class TestCleanNormalize(unittest.TestCase):
+    def test_normalize_amount(self):
+        self.assertEqual(normalize_amount("1,000"), 1000.0)
+
+    def test_normalize_phone(self):
+        self.assertEqual(normalize_phone(" 0788123456 "), "0788123456")
+
+
+if __name__ == "__main__":
+    unittest.main()
